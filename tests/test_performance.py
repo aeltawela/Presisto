da5f185a266a69bf2dcfa3ed4008e34926058b0d
bakabiko <ahmedabt@gmail.com>
2017-06-17 16:49:07 +0200

    Updated test_performance script

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -3,6 +3,8 @@ from sys import path
 path.extend("./")
 from presisto.presistentdict import PersistentDict
 from time import time
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
 
 iterations = 1000000
 pd = PersistentDict()
@@ -25,4 +27,38 @@ start_time = time()
 for i in range(iterations):
     pd[i]
 
+print("read {} records in {} seconds ".format(iterations, time() - start_time))
+
+print("----- trying concurrent I/O -----")
+
+
+def write(key, value, pd):
+    pd[key] = value
+
+
+def read(key, pd):
+    return pd[key]
+
+
+def check(key, pd):
+    return key in pd
+
+
+pd = PersistentDict('./.data2')
+with ThreadPoolExecutor(max_workers=cpu_count() * 2) as executor:
+    start_time = time()
+    for i in range(iterations):
+        executor.submit(write, i, i, pd)
+print("Stored {} records in {} seconds ".format(iterations, time() - start_time))
+
+with ThreadPoolExecutor(max_workers=cpu_count() * 2) as executor:
+    start_time = time()
+    for i in range(iterations):
+        executor.submit(check, i, pd)
+print("Check existance of {} records in {} seconds ".format(iterations, time() - start_time))
+
+with ThreadPoolExecutor(max_workers=cpu_count() * 2) as executor:
+    start_time = time()
+    for i in range(iterations):
+        executor.submit(read, i, pd)
 print("read {} records in {} seconds ".format(iterations, time() - start_time))
